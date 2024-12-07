c8b32c5c72b302214726f3f7078519c35fb598f4
CuongChuck <englishtalent123@gmail.com>
2024-12-07 21:06:17 +0700

    Update Routes

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,9 +1,11 @@
+import ChoosePayment from './pages/ChoosePayment';
 import Home from './pages/Home';
 import SelectNumPage from './pages/SelectNumPage';
 
 const routes = [
     { path: '/', component: Home },
-    { path: '/buy', component: SelectNumPage }
+    { path: '/buy', component: SelectNumPage },
+    { path: '/payment-method', component: ChoosePayment }
 ];
 
 export { routes };
