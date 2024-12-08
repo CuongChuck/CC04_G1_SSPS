7a07dbb4d2651eb2690b4806a450102f0b60c4fc
Bùi Vũ Thiên Đăng <166787112+DangBuiVT@users.noreply.github.com>
2024-12-09 00:56:09 +0700

    Update NavBar.jsx

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,84 +1,42 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import dropdownlogo from "../assets/drop_down_menu.svg";
-import userIcon from "../assets/user_icon.svg";
-import noti from "../assets/noti-bell.svg";
-import message from "../assets/message.svg";
-import "./NavBar.css";
+import React from 'react';
+import './NavBar.css';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import ChatRoundedIcon from '@mui/icons-material/ChatRounded';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { useNavigate } from 'react-router-dom';
 
-function NavBar({ username, id }) {
-  const [isActive, setActive] = useState(false);
+const NavBar = () => {
+    const navigate = useNavigate();
+    return (
+        <div className='NavBarContainer'>
+            <IconButton
+                size='small'
+                color='black'
+                style={{
+                    backgroundColor:'#D9D9D9',
+                    height:'30px',
+                    borderRadius:'3px',
+                    margin:'15px 15px'
+                }}
+                >
+                    <MenuIcon />
+            </IconButton>
+            <a className="NavLink" href='/'>DANH MỤC</a>
+            <a className="NavLink" href='#'>IN</a>
+            <a className="NavLink" href='/buy'>MUA GIẤY</a>
+            <div className='IconContainer'>
+                <NotificationsIcon fontSize='large' style={{display:'flex', color:'#CED4DA'}} />
+                <ChatRoundedIcon fontSize='large' style={{color:'#CED4DA'}} />
+                <AccountCircleIcon
+                    fontSize='large'
+                    style={{color:'#CED4DA'}}
+                    onClick={() => {navigate('/login/role')}}
+                />
+            </div>
+            </div>
+            )
+        }
 
-  const handleClick = (tab) => {
-    setActive(tab);
-  };
-
-  return (
-    <div className="header">
-      <nav className="navbar">
-        <div className="dropdown-mini">
-          <button className="drop-btn">
-            <img src={dropdownlogo} alt="Dropdown Menu" className="drop" />
-          </button>
-        </div>
-        {["trang chủ", "in giấy", "mua giấy"].map((tab) => {
-          if (tab === "mua giấy") {
-            return (
-              <Link
-                key={tab}
-                to="/buy"
-                className={`nav-item ${isActive === tab ? "active" : ""}`}
-              >
-                {tab.toUpperCase()}
-              </Link>
-            );
-          }
-
-          if (tab === "trang chủ") {
-            return (
-              <Link
-                key={tab}
-                to="/"
-                className={`nav-item ${isActive === tab ? "active" : ""}`}
-              >
-                {tab.toUpperCase()}
-              </Link>
-            );
-          }
-
-          return (
-            <button
-              key={tab}
-              className={`nav-item ${isActive === tab ? "active" : ""}`}
-              onClick={() => handleClick(tab)}
-            >
-              {tab.toUpperCase()}
-            </button>
-          );
-        })}
-      </nav>
-      <div className="header-right">
-        <button>
-          <img src={noti} alt="Notifications" className="noti right-item" />
-        </button>
-        <button>
-          <img src={message} alt="Messages" className="msg right-item" />
-        </button>
-        <div className="header-user-info right-item">
-          <div className="username">Tên người dùng: {username}</div>
-          <div className="user-id">Mã người dùng: {id}</div>
-        </div>
-        <button>
-          <img
-            src={userIcon}
-            alt="User Icon"
-            className="user-icon right-item"
-          />
-        </button>
-      </div>
-      <div className="spss">SPSS</div>
-    </div>
-  );
-}
-
-export default NavBar;
+        export default NavBar
