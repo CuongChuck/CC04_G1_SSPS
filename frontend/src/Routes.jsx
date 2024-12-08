d58e8f01e9019d817fd05dd29818535a6b7eabc5
CuongChuck <englishtalent123@gmail.com>
2024-12-08 22:00:10 +0700

    Update

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -3,9 +3,11 @@ import Home from './pages/Home';
 import SelectNumPage from './pages/SelectNumPage';
 import Review from './pages/Review';
 import Error from './pages/Error';
+import Login from './pages/Login';
 
 const routes = [
     { path: '/', component: Home },
+    { path: '/login', component: Login },
     { path: '/buy', component: SelectNumPage },
     { path: '/payment-method', component: ChoosePayment },
     { path: '/review', component: Review },
