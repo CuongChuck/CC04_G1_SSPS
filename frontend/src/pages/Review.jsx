f44cb4433755f164a75c4b97e06d497172438fe0
Bùi Vũ Thiên Đăng <166787112+DangBuiVT@users.noreply.github.com>
2024-12-09 01:37:15 +0700

    Update Review.jsx

--- frontend/src/pages/Review.jsx
+++ frontend/src/pages/Review.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Review.css";
+import Checkmark from "../assets/green-icon.svg";
 
 const Review = () => {
   return (
     <div className="review-container">
+      <img src={Checkmark} className="bgr-check" />
       <div className="message">
         <p>Giao dịch thành công</p>
         <p>Cảm ơn bạn đã mua</p>
