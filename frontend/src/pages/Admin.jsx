7abad9a3f9def7f8edf142e8aa0bc4dd1fb99c20
CuongChuck <87120150+CuongChuck@users.noreply.github.com>
2024-12-08 23:51:34 +0700

    Update Admin.jsx

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Admin.css';
+import { useNavigate } from 'react-router-dom';
 
 const Admin = () => {
+    const navigate = useNavigate();
     return (
         <div>
-            <div className="block">Quản lý lịch sử giao dịch</div>
+            <div className="block" onClick={() => {navigate('/history-admin')}>
+                Quản lý lịch sử giao dịch
+            </div>
             <div className="block">Quản lý giấy</div>
         </div>
     )
