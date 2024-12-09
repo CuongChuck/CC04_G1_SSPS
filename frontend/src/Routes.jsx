46d83ff2ea703015ffad87472a2f4b0138c68cb0
CuongChuck <englishtalent123@gmail.com>
2024-12-09 07:19:19 +0700

    Fix bugs

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -10,7 +10,7 @@ import LoginRole from './pages/LoginRole';
 import LoginAdmin from './pages/LoginAdmin';
 import PaperManager from './pages/PaperManager';
 import Admin from './pages/Admin';
-import ConfirmPurchase from "./pages/confirm-purchase";
+import ConfirmPurchase from "./pages/Confirm-purchase";
 import OtpVerify from "./pages/otp-verifying";
 
 const routes = [
